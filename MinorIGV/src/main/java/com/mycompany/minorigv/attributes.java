42129552420b236899c702c5a64ae05e9e485593
Anne <anne_van_ewijk@hotmail.com>
2018-11-21 15:25:58 +0100

    Commentaar bij gffReader en attributes

--- MinorIGV/src/main/java/com/mycompany/minorigv/attributes.java
+++ MinorIGV/src/main/java/com/mycompany/minorigv/attributes.java
@@ -2,29 +2,38 @@ package com.mycompany.minorigv;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Kolom 8 van het ingeladen bestand bevat verdere informatie over het element. Deze informatie wordt in een hashmap gezet
+ * en zo opgeslagen in het object. Hierdoor kan deze informatie altijd opgehaald worden. Per regel van het bestand wordt deze
+ * class aangeroepen.
+ *
+ */
 public class attributes {
     Map<String, Object> attribute = new HashMap<String, Object>();
 
+    /**
+     * De kolom 8 (attributen) wordt gesplit waarna de informatie wordt opgeslagen in een hashmap.
+     * Deze hashmap wordt gereturned zodat deze informatie ook opgeslagen kan worden in het object.
+     *
+     * @param attr     Kolom 8 uit de ingeladen bestanden die informatie van element bevat.
+     * @return         Er wordt een hashmap gereturned verdere informatie over het element.
+     */
     public HashMap splitAtt(String attr){
-
-
+        // Alle informatie in kolom 8 is gescheiden doormiddel van ;.
         String[] subAttr = attr.split(";");
+
+        // Er wordt een hashmap gemaakt van de informatie van het element.
         for(String s: subAttr){
             String[] split_s = s.split("=");
             attribute.put(split_s[0], split_s[1]);
         }
 
-        for (String name: attribute.keySet()){
-
-            String key =name.toString();
-            String value = attribute.get(name).toString();
-        }
-
-
+        // Als de hashmap een key heeft Dbxref, word er gekeken of er een "," aanwezig is.
         if (attribute.containsKey("Dbxref")){
             Map<String, String> dbxref_ids = new HashMap<String, String>();
             String line = (String) attribute.get("Dbxref");
 
+            // Een "," geef aan dat er meerdere IDs zijn. Er wordt een nieuwe hashmap gemaakt met deze IDs.
             if(line.contains(",")){
                 String[] splitIDs = line.split(",");
                 for(String ID: splitIDs){
