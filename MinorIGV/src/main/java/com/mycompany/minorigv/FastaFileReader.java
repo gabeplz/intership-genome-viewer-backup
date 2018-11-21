2523acf3db684e53249eab1002f751257cf8a61f
TimKuijpers <tim_kuijpers@live.nl>
2018-11-21 15:22:53 +0100

    bob the string builder

--- MinorIGV/src/main/java/com/mycompany/minorigv/FastaFileReader.java
+++ MinorIGV/src/main/java/com/mycompany/minorigv/FastaFileReader.java
@@ -1,50 +1,52 @@
 package com.mycompany.minorigv;
 import java.io.*;
+import java.util.ArrayList;
+
 public class FastaFileReader {
 
-    public String seq (String fastafile)throws IOException {
-        System.out.println("path here"+fastafile);
-        BufferedReader br = null;
-        FileReader fr = null;
-        String sequentie = "";
+    //String pad = "C:\\Users\\Tim Kuijpers\\Desktop\\Minor_project\\git\\Minor-IGV\\MinorIGV\\resources";
+
+
+    public static ArrayList<String> getSequences(String pad) throws IOException{
+
+        BufferedReader f_reader = new BufferedReader(new FileReader(pad));
+        ArrayList<String> CH_list = new ArrayList<>();
+
+        String regel = f_reader.readLine();
+
+        while(regel != null){
+            String header = regel;
+            regel = f_reader.readLine();
+
+            StringBuilder bob = new StringBuilder();
+
+            while(regel != null && !regel.startsWith(">")){
+                regel = regel.trim();
+                bob.append(regel);
+                regel = f_reader.readLine();
+            }
+
+            System.out.println(header);
+            CH_list.add(bob.toString());
+
+
+        }
+        System.out.println(CH_list);
+        return CH_list;
+    }
+/*
+    public static void main(String[] args) {
+        String pad = "C:\\Users\\Tim Kuijpers\\Desktop\\Minor_project\\git\\Minor-IGV\\MinorIGV\\resources\\GCF_000146045.2_R64_genomic.fna";
 
         try {
+            System.out.println(getSequences(pad).get(0).length());
+        } catch(Exception e){
+            System.out.println(e.toString());
+        }
 
-            //br = new BufferedReader(new FileReader(FILENAME));
-            fr = new FileReader(fastafile);
-            br = new BufferedReader(fr);
-
-            String sCurrentLine;
-
-            while ((sCurrentLine = br.readLine()) != null) {
-
-                if (!sCurrentLine.contains(">")){sequentie+=sCurrentLine;}
-
-            }
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        } finally {
-
-            try {
-
-                if (br != null)
-                    br.close();
-
-                if (fr != null)
-                    fr.close();
-
-            } catch (IOException ex) {
-
-                ex.printStackTrace();
-
-            }
-
-        }return sequentie;
 
     }
 
+*/
 
 }
