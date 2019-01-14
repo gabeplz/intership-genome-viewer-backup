8da488b304c7e49b50a3e52b9f7d813be06c8a3d
TimKuijpers <tim_kuijpers@live.nl>
2019-01-14 11:59:17 +0100

    move save orf to new class

--- MinorIGV/src/main/java/com/mycompany/minorigv/gui/IGVMenuBar.java
+++ MinorIGV/src/main/java/com/mycompany/minorigv/gui/IGVMenuBar.java
@@ -239,72 +239,76 @@ public class IGVMenuBar extends JMenuBar {
      * Bij de RadioButton kan er gekozen worden of gezocht wordt over het hele sequentie of tussen een bepaalde start en stop.
      */
     private void saveOrfAction() {
-        // Radio Button wordt aangemaakt.
-        buttonAll = new JRadioButton("All", true);
-        buttonBetween = new JRadioButton("Between");
-        ButtonGroup groupRadioButton = new ButtonGroup();
-        groupRadioButton.add(buttonAll);
-        groupRadioButton.add(buttonBetween);
-
-        // Label wordt aangemaakt.
-        final JLabel labelLengthORFUser = new JLabel("Length ORF (nt): ", JLabel.LEFT);
-
-        // Er kunnen geen letters ingevoerd worden, wanneer dit wel gebeurd wordt het vorige cijfer gebruikt.
-        NumberFormat format = NumberFormat.getInstance();
-        NumberFormatter formatter = new NumberFormatter(format);
-        formatter.setValueClass(Integer.class);
-        formatter.setMinimum(0);
-        formatter.setMaximum(Integer.MAX_VALUE);
-        formatter.setAllowsInvalid(true);
-        textField = new JFormattedTextField(formatter);
-        textField.setValue(100); // Strandaard lengte van het ORF.
-
-        // Save button wordt aangemaakt.
-        JButton saveButton = new JButton();
-        saveButton.setText("Save");
-
-        // Panel voor Radio Button, Label en Textfield wordt aangemaakt
-        JPanel panel = new JPanel(new GridLayout(2, 2));
-        panel.add(buttonAll);
-        panel.add(buttonBetween);
-        panel.add(labelLengthORFUser);
-        panel.add(textField);
-
-        // Panel voor de Button wordt aangemaakt.
-        JPanel panelForButton = new JPanel();
-        panelForButton.add(saveButton);
-
-        // Er wordt een Padding ingesteld.
-        panel.setBorder(new EmptyBorder(10, 10, 10, 10));
-
-        // Er wordt een frame aangemaakt.
-        JFrame f = new JFrame();
-        f.getContentPane().setLayout(new BoxLayout(f.getContentPane(), BoxLayout.PAGE_AXIS));
-        f.getContentPane().add(panel);
-        f.getContentPane().add(panelForButton);
-        f.pack();
-        f.setLocationRelativeTo(null);
-        f.setVisible(true);
-
-        // ActionListener voor de Save Button
-        saveButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    saveButtonAction();
-                    f.dispose();
-                } catch (FileNotFoundException e1) {
-                    e1.printStackTrace();
-                } catch (UnsupportedEncodingException e1) {
-                    e1.printStackTrace();
-                } catch (NullPointerException el) {
-                    final JFrame frame = new JFrame();
-                    JOptionPane.showMessageDialog(frame, "Er is geen ORF om op te slaan ");
-                    frame.dispose();
-                }
-            }
-        });
+        SaveOrf orf = new SaveOrf();
+        orf.setContext(cont);
+        orf.saveOrfAction();
     }
+//        // Radio Button wordt aangemaakt.
+//        buttonAll = new JRadioButton("All", true);
+//        buttonBetween = new JRadioButton("Between");
+//        ButtonGroup groupRadioButton = new ButtonGroup();
+//        groupRadioButton.add(buttonAll);
+//        groupRadioButton.add(buttonBetween);
+//
+//        // Label wordt aangemaakt.
+//        final JLabel labelLengthORFUser = new JLabel("Length ORF (nt): ", JLabel.LEFT);
+//
+//        // Er kunnen geen letters ingevoerd worden, wanneer dit wel gebeurd wordt het vorige cijfer gebruikt.
+//        NumberFormat format = NumberFormat.getInstance();
+//        NumberFormatter formatter = new NumberFormatter(format);
+//        formatter.setValueClass(Integer.class);
+//        formatter.setMinimum(0);
+//        formatter.setMaximum(Integer.MAX_VALUE);
+//        formatter.setAllowsInvalid(true);
+//        textField = new JFormattedTextField(formatter);
+//        textField.setValue(100); // Strandaard lengte van het ORF.
+//
+//        // Save button wordt aangemaakt.
+//        JButton saveButton = new JButton();
+//        saveButton.setText("Save");
+//
+//        // Panel voor Radio Button, Label en Textfield wordt aangemaakt
+//        JPanel panel = new JPanel(new GridLayout(2, 2));
+//        panel.add(buttonAll);
+//        panel.add(buttonBetween);
+//        panel.add(labelLengthORFUser);
+//        panel.add(textField);
+//
+//        // Panel voor de Button wordt aangemaakt.
+//        JPanel panelForButton = new JPanel();
+//        panelForButton.add(saveButton);
+//
+//        // Er wordt een Padding ingesteld.
+//        panel.setBorder(new EmptyBorder(10, 10, 10, 10));
+//
+//        // Er wordt een frame aangemaakt.
+//        JFrame f = new JFrame();
+//        f.getContentPane().setLayout(new BoxLayout(f.getContentPane(), BoxLayout.PAGE_AXIS));
+//        f.getContentPane().add(panel);
+//        f.getContentPane().add(panelForButton);
+//        f.pack();
+//        f.setLocationRelativeTo(null);
+//        f.setVisible(true);
+//
+//        // ActionListener voor de Save Button
+//        saveButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                try {
+//                    saveButtonAction();
+//                    f.dispose();
+//                } catch (FileNotFoundException e1) {
+//                    e1.printStackTrace();
+//                } catch (UnsupportedEncodingException e1) {
+//                    e1.printStackTrace();
+//                } catch (NullPointerException el) {
+//                    final JFrame frame = new JFrame();
+//                    JOptionPane.showMessageDialog(frame, "Er is geen ORF om op te slaan ");
+//                    frame.dispose();
+//                }
+//            }
+//        });
+//    }
 
     /**
      * Wanneer er op de Save Button in het panel wordt geklikt, worden de gegevens opgehaald die op dat moment zijn ingevoerd.
