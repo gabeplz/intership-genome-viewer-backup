449dfac5685472d1a6784cba45a8708a86db2057
amberr098 <amberr098.jg@hotmail.com>
2019-01-14 14:24:59 +0100

    BLASTp

--- MinorIGV/src/main/java/com/mycompany/minorigv/sequence/BlastORF.java
+++ MinorIGV/src/main/java/com/mycompany/minorigv/sequence/BlastORF.java
@@ -1,23 +1,33 @@
 package com.mycompany.minorigv.sequence;
 
+import com.mycompany.minorigv.blast.BlastOutput;
+import com.mycompany.minorigv.blast.ObjectFactory;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.validation.SchemaFactory;
 
 public class BlastORF {
 
     private String fasta = "/NAS/minor-g1/non_redundant/blast.fasta";
-    private String out = "/NAS/minor-g1/non_redundant/out.xml";
+    private String out = "/NAS/minor-g1/non_redundant/results.out";
     private String blastDB = "/NAS/minor-g1/non_redundant/nr"; // roep nr dan aan
     public void run() throws IOException{
        // File fasta=File.createTempFile("blast", ".fa");
         File blast=File.createTempFile("blast", ".xml");
         try
         {
-
+/*
 
             ProcessBuilder pb = new ProcessBuilder(
                     "blastp",
@@ -25,6 +35,8 @@ public class BlastORF {
                     "-num_threads","8",
                     "-query",fasta,
                     "-evalue", "1e-5",
+                    "-outfmt", "5",
+                    "-max_target_seqs", "1",
                     "-out",out
             );
 
@@ -33,16 +45,24 @@ public class BlastORF {
 
             Process proc = pb.start();
             if(proc.waitFor()!=0) throw new RuntimeException("error occured");
-            JAXBContext jc = JAXBContext.newInstance("blast");
+*/
+            JAXBContext jc = JAXBContext.newInstance("com.mycompany.minorigv.blast");
+
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            spf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            spf.setFeature("http://xml.org/sax/features/validation", false);
+
+            XMLReader xmlReader = spf.newSAXParser().getXMLReader();
+            InputSource inputSource = new InputSource(
+                    new FileReader(out));
+            SAXSource source = new SAXSource(xmlReader, inputSource);
 
             Unmarshaller u = jc.createUnmarshaller();
-            u.setSchema(null);
-
-
 
             /** read the result */
 
-           // return  (blast.BlastOutput)u.unmarshal(blast);
+            BlastOutput bo = (BlastOutput) u.unmarshal(source);
+            System.out.println(bo.getBlastOutputIterations().getIteration().get(3).getIterationHits().getHit().get(0).getHitHsps().getHsp().get(0).getHspEvalue());
 
         }
         catch(Exception err)
@@ -52,7 +72,7 @@ public class BlastORF {
         }
         finally
         {
-            blast.delete();
+            //blast.delete();
             //fasta.delete();
         }
     }
